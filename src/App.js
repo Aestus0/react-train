92e69054b40ed2c55b64b0e79ae83b165e8cdac6
colin <403622098@qq.com>
2019-12-13 14:35:30 +0800

    init

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Test extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {a: 'test2'};
+    this.changeTest = this.changeTest.bind(this);
+  }
+
+  changeTest() {
+    setTimeout(()=>this.setState({a: 'abc'}), 2000);
+  }
+
+  render() {
+    return (<span>{ this.props.test + '2'}</span>);
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Test test={"tet2"}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/TodoList/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TaskList from "./TaskList";
+
+export default class TodoList extends React.Component{
+    render() {
+        return (<div>
+            <TaskList/>
+        </div>)
+    }
+}
